77a25fdf1573b87a0b454a9b4c8f485edc2b0767
Vagner Santos <eu@vagnersantos.com>
2016-09-24 19:19:06 -0300

    Class 4

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,3 +1,4 @@
+/*
 var tdPeso1 = document.getElementById("peso-1");
 var tdAltura1 = document.getElementById("altura-1");
 
@@ -10,18 +11,23 @@ var tdAltura2 = document.getElementById("altura-2");
 
 //nomes podem ser string como altura ou nome mesmo como em peso
 var paciente2 = {peso: tdPeso2.textContent, "altura": tdAltura2.textContent};
-
 var pacientes = [paciente1, paciente2];
+*/
 
-var i = 0;
+var trsPacientes = document.getElementsByClassName('paciente');
 
-while (i <= pacientes.length - 1) {
-  var paciente = pacientes[i];
+for (var i = 0; i <= trsPacientes.length - 1; i++ ) {
+  var trPaciente = trsPacientes[i];
+
+  var paciente = {
+        nome:   trPaciente.getElementsByClassName("info-nome")[0].textContent,
+        peso:   trPaciente.getElementsByClassName("info-peso")[0].textContent,
+        altura: trPaciente.getElementsByClassName("info-altura")[0].textContent };
 
   if (paciente.altura != 0) {
     var imc = paciente.peso / (paciente.altura * paciente.altura);
-    var tdImc = document.getElementById("imc-2");
 
+    var tdImc = trPaciente.getElementsByClassName("info-imc")[0];
     tdImc.textContent = imc;
 
     console.log(imc);
@@ -30,5 +36,4 @@ while (i <= pacientes.length - 1) {
     console.log("Altura não pode ser 0!");
   }
 
-  i++;
 }
